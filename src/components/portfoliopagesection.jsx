7f3ddcae9bb04a2549cf0dbfc1a5970a5a70d5cc
Ajaysharma <akpancholi8@gmail.com>
2025-10-18 16:35:34 +0530

    commit

--- src/components/portfoliopagesection.jsx
+++ src/components/portfoliopagesection.jsx
@@ -22,8 +22,8 @@ const Portfoliopagesection = () => {
                             </div>
                         </div>
                         <div className="col-span-2 h-full">
-                            <div className="pb-5"><img className='' src="/assets/Home Decor Hacks For Your Wedding.jpg" alt="" /></div>
-                            <div className=""><img className=' ' src="/assets/Chicken Milega Kya_.jpg" alt="" /></div>
+                            <div className="pb-5"><img className='' src="/image 1 (12).png" alt="" /></div>
+                            <div className=""><img className=' ' src="/image 2 (9).png" alt="" /></div>
                         </div>
                     </div>
                 </div>
@@ -31,17 +31,17 @@ const Portfoliopagesection = () => {
                 <div className="md:hidden block">
                     <div className="max-w-[1240px]  mx-auto  gap-5">
                         <div className="space-y-5 w-fit">
-                            <div className=""><img src="/Elegant Wedding Decorations.svg" alt="" /></div>
-                            <div className=""><img src="/Manavarai Decoration - Wedding Planners in Coimbatore Tamilnadu India.svg" alt="" /></div>
+                            <div className=""><img className='mx-auto' src="/Elegant Wedding Decorations.svg" alt="" /></div>
+                            <div className=""><img className='mx-auto' src="/Manavarai Decoration - Wedding Planners in Coimbatore Tamilnadu India.svg" alt="" /></div>
 
-                            <div className=""><img src="/latest wedding ceremony stage decoration ideas.svg" alt="" /></div>
-                            <div className=""><img src="/Low Budget Wedding Stage Decoration Ideas - For Indian Weddings.svg" alt="" /></div>
+                            <div className=""><img className='mx-auto' src="/latest wedding ceremony stage decoration ideas.svg" alt="" /></div>
+                            <div className=""><img className='mx-auto' src="/Low Budget Wedding Stage Decoration Ideas - For Indian Weddings.svg" alt="" /></div>
                             <div>
-                                <img src="/Anil Caterers - Wedding Caterers In Bangalore.svg" alt="" />
+                                <img className='mx-auto' src="/Anil Caterers - Wedding Caterers In Bangalore.svg" alt="" />
                             </div>
 
-                            <div className=""><img className='' src="/Home Decor Hacks For Your Wedding (1).svg" alt="" /></div>
-                            <div className=""><img className='' src="/Chicken Milega Kya_ (1).svg" alt="" /></div>
+                            <div className=""><img className='mx-auto'  src="/Home Decor Hacks For Your Wedding (1).svg" alt="" /></div>
+                            <div className=""><img className='mx-auto'  src="/Chicken Milega Kya_ (1).svg" alt="" /></div>
                         </div>
                     </div>
                 </div>
